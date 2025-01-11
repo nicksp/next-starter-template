f1b09517a012f35213c3d1f297ba620901d7922f
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-11 22:03:02 +0100

    Enable some ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,14 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
+  // Make sure to put 'prettier' last, so it gets the chance to override other configs
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+    },
+  },
 ]
 
 export default eslintConfig
