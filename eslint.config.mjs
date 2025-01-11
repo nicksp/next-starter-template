d53881ce9dd34a95c6c6dc93d7c8af2166938cf0
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-11 23:59:57 +0100

    Enforce consistent files and folders naming pattern

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import checkFile from 'eslint-plugin-check-file'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -8,9 +9,28 @@ const eslintConfig = [
   // Make sure to put 'prettier' last, so it gets the chance to override other configs
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
+    files: ['src/**/*'],
+    plugins: {
+      'check-file': checkFile,
+    },
     rules: {
       'prefer-arrow-callback': ['error'],
       'prefer-template': ['error'],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{ts,tsx}': 'KEBAB_CASE',
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          'src/**': 'KEBAB_CASE',
+        },
+      ],
     },
   },
 ]
