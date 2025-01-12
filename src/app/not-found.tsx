5307eb6aaf2e0370f95a2706a919b97f1b184e9a
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-12 21:46:15 +0100

    Add Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody } from '@nextui-org/react'
+import { IconDeviceUnknown } from '@tabler/icons-react'
+
+export default function NotFound() {
+  return (
+    <Card className="mx-auto mt-4 max-w-md">
+      <CardBody>
+        <p className="flex items-center justify-center gap-2 text-2xl">
+          <IconDeviceUnknown />
+          This page cannot be found
+        </p>
+      </CardBody>
+    </Card>
+  )
+}
