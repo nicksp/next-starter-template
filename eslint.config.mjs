9c194820d5c7b8c97bd1c37a2c9ed8021924293e
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-13 22:20:13 +0100

    Setup NextAuth authentication with Google provider

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,7 @@ const eslintConfig = [
       'check-file/folder-naming-convention': [
         'error',
         {
-          'src/**': 'KEBAB_CASE',
+          'src/**/!^[.*': 'KEBAB_CASE',
         },
       ],
     },
--- src/env/server.ts
+++ src/env/server.ts
@@ -4,6 +4,8 @@ import { type ZodError, z } from 'zod'
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'production']),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
   onValidationError: (error: ZodError) => {
     console.error(
