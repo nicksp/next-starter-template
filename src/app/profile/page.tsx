074c1d52c2421c95f357492aeb81081fc94919d9
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-14 01:22:29 +0100

    Create Profile page with a user card

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,22 @@
+import { Card, CardBody, User } from '@nextui-org/react'
+import { getServerSession } from 'next-auth'
+
+import options from '@/config/auth'
+
+export default async function ProfilePage() {
+  const session = await getServerSession(options)
+  return (
+    <Card className="mx-auto mt-4 max-w-md">
+      <CardBody>
+        <User
+          avatarProps={{
+            showFallback: !session?.user?.image,
+            src: session?.user?.image ?? '',
+          }}
+          description={session?.user?.email}
+          name={session?.user?.name}
+        />
+      </CardBody>
+    </Card>
+  )
+}
